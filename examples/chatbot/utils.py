b608f34f2f75126e4d8506c895c9b6975dca260e
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-04-27 14:51:08 -0700

    updates

---
+++ examples/chatbot/utils.py
@@ -0,0 +1,15 @@
+import torch
+
+
+USE_CUDA = torch.cuda.is_available()
+device = torch.device("cuda" if USE_CUDA else "cpu")
+
+corpus_name = "cornell movie-dialogs corpus"
+corpus = os.path.join("data", corpus_name)
+
+def printLines(file, n=10):
+    with open(file, 'rb') as datafile:
+        lines = datafile.readlines()
+    for line in lines[:n]:
+        print(line)
+
