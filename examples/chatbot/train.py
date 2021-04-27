1ece8d53f31ca87c24defa929336806b2e783ce9
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-04-27 15:01:47 -0700

    updates

--- examples/chatbot/train.py
+++ examples/chatbot/train.py
@@ -8,12 +8,8 @@ from torch.jit import script, trace
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
-import csv
 import random
-import re
-import os
 import unicodedata
-import codecs
 from io import open
 import itertools
 import math
--- examples/chatbot/utils.py
+++ examples/chatbot/utils.py
@@ -1,16 +1,6 @@
 import torch
-import os
 
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
 
-corpus_name = "cornell movie-dialogs corpus"
-corpus = os.path.join("data", corpus_name)
-
-# for showing the raw data
-def printLines(file, n=10):
-    with open(file, 'rb') as datafile:
-        lines = datafile.readlines()
-    for line in lines[:n]:
-        print(line)
 
