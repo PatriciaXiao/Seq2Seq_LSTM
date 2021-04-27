959a817e8deffdef24e332575e3ada99d9b89555
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-04-27 15:09:44 -0700

    updates (data loaded)

--- examples/chatbot/train.py
+++ examples/chatbot/train.py
@@ -8,12 +8,21 @@ from torch.jit import script, trace
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
+import os
 import random
-import unicodedata
 from io import open
 import itertools
 import math
 
 
+from data_loader import loadPrepareData
+
 from utils import *
 
+# Load/Assemble voc and pairs
+save_dir = os.path.join("data", "save")
+voc, pairs = loadPrepareData(corpus, corpus_name, datafile, save_dir)
+# Print some pairs to validate
+print("\npairs:")
+for pair in pairs[:10]:
+    print(pair)
--- examples/chatbot/utils.py
+++ examples/chatbot/utils.py
@@ -1,6 +1,34 @@
 import torch
+import os
+import re
+import unicodedata
 
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
 
+corpus_name = "cornell movie-dialogs corpus"
+corpus = os.path.join("data", corpus_name)
+datafile = os.path.join(corpus, "formatted_movie_lines.txt")
 
+# Default word tokens
+PAD_token = 0  # Used for padding short sentences
+SOS_token = 1  # Start-of-sentence token
+EOS_token = 2  # End-of-sentence token
+
+MAX_LENGTH = 10  # Maximum sentence length to consider
+
+# Turn a Unicode string to plain ASCII, thanks to
+# https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# Lowercase, trim, and remove non-letter characters
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    s = re.sub(r"\s+", r" ", s).strip()
+    return s
