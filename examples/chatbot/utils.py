709e1bdbc054a7f866613f0ce5d07ab2c367ebcb
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-04-27 14:52:46 -0700

    updates

--- examples/chatbot/utils.py
+++ examples/chatbot/utils.py
@@ -7,6 +7,7 @@ device = torch.device("cuda" if USE_CUDA else "cpu")
 corpus_name = "cornell movie-dialogs corpus"
 corpus = os.path.join("data", corpus_name)
 
+# for showing the raw data
 def printLines(file, n=10):
     with open(file, 'rb') as datafile:
         lines = datafile.readlines()
